1f640fcdcf4b49d947fbb81f6dc1b85afde56717
Sathyannsp <60613739+Sathyannsp@users.noreply.github.com>
2026-05-16 20:42:38 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            // Check if needed number already exists
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Store current number and index
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
